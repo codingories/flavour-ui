30f8d27a0c7017167faaf841a1de08df4314be39
朱骏杰 <ories.zhu@mochain.vip>
2020-09-20 12:09:42 +0800

    初始化vue-router

---
+++ src/components/Frank.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>你好，我是第一个组件</h1>
+</template>
+
+<script>
+export default {
+  name: 'Frank',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
