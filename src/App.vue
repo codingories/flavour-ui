7b297bf8f50d3f1b7bf89d7133919b4277f353c7
朱骏杰 <ories.zhu@mochain.vip>
2020-09-20 22:35:18 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <router-view/>
 </template>
 
-<script>
+<script lang="ts">
+import {ref, provide} from 'vue'
 export default {
   name: 'App',
-  components: {
+  setup(){
+    const menuVisible = ref(false); //  ref理解成盒子,里面有个值false
+    provide('xxx', menuVisible) // set,标记为所有子组件都可以用
   }
 }
 </script>
