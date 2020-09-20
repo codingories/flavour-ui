a478a3b6dd1ba1a8d3e1a5e76f99d6c12e29106f
朱骏杰 <ories.zhu@mochain.vip>
2020-09-20 23:02:51 +0800

    完善aside逻辑

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@ import {ref, provide} from 'vue'
 export default {
   name: 'App',
   setup(){
-    const menuVisible = ref(false); //  ref理解成盒子,里面有个值false
-    provide('menuVisible', menuVisible) // set,标记为所有子组件都可以用
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 800 ? false : true);
+    provide("menuVisible", menuVisible); // set
   }
 }
 </script>
