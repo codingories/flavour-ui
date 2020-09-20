fcff4e91b7354aba513ac0511cfe6be53f42f968
朱骏杰 <ories.zhu@mochain.vip>
2020-09-20 22:58:14 +0800

    按钮出现并可以响应切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 
@@ -43,7 +43,6 @@
     }
   }
   > .toggleAside {
-    display: inline-block;
     width: 24px;
     height: 24px;
     background: red;
@@ -51,10 +50,14 @@
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
+    display: none;
   }
-  @media (max-width:500px) {
+  @media (max-width:800px) {
     > .menu{display: none;}
     > .logo{margin: 0 auto;}
+    > .toggleAside {
+      display: inline-block;
+    }
   }
 }
 </style>
