87808729a591fa323cfaf4e1522b65f671c88dbb
codingories <ories.zhu@mochain.vip>
2020-10-04 21:15:44 +0800

    通过$attrs将全部属性事件绑定到指定的某元素

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,13 +3,24 @@
     <div>button 示例</div>
     <h1>示例1</h1>
     <div>
-      <Button>你好</Button>
+      <Button @click="onClick"
+              @focus="onClick"
+              @mouseover="onClick"
+              size="small"
+      >你好</Button>
+<!--      Vue 直接帮我们把onClick传到了根元素-->
     </div>
   </div>
 </template>
 <script lang="ts">
   import Button from '../lib/Button.vue'
   export default {
-    components: {Button :Button}
+    components: {Button},
+    setup(){
+      const onClick = ()=>{
+        console.log('hi')
+      };
+      return {onClick}
+    }
   }
 </script>
