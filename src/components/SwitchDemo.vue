6816f91646c2d188d6d070ed875f7a87a70e4eb5
codingories <ories.zhu@mochain.vip>
2020-10-04 10:34:34 +0800

    优化样式

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
     <Switch v-model:value="y" ></Switch>
-
-<!--    @update:value="y = $event"-->
-<!--    @input表示变化的时候让y为最新的值, 最新的值就是$event-->
+    <!--   v-model等于就是简化了这句话 @update:value="y = $event"-->
   </div>
 </template>
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import { ref } from 'vue'
-export default {
-  components: {Switch},
-  setup(){
-    const y = ref(false)
-    return {y}
-    // 给value 默认值y ，y为true ，
+  import Switch from '../lib/Switch.vue'
+  import { ref } from 'vue'
+  export default {
+    components: {Switch},
+    setup(){
+      const y = ref(false);
+      return {y}
+      // 给value 默认值y ，y为true ，
+    }
   }
-}
 </script>
