8c8fac7e16a88aa591e89bfe1787d6d2332fcdc6
codingories <ories.zhu@mochain.vip>
2020-10-05 10:00:31 +0800

    支持level

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,39 +1,36 @@
 <template>
-  <button class="gulu-button"
-          :class="classes"
-  >
+  <button class="gulu-button" :class="classes">
     <slot />
   </button>
 </template>
 <script lang="ts">
-  import { computed } from 'vue';
+  import { computed } from "vue";
   export default {
     props: {
       theme: {
-        type:String,
-        default: 'button'
+        type: String,
+        default: "button",
       },
       size: {
         type: String,
-        default: 'normal'
-      }
+        default: "normal",
+      },
+      level: {
+        type: String,
+        default: "normal",
+      },
     },
-    setup(props){
-      const {theme,size} = props
-      const classes = computed(()=>{
-        // Vue3的计算属性
-        console.log('arg',
-          { [`gulu-theme-${theme}`]: theme,
-            [`gulu-theme-${size}`]: size}
-        )
+    setup(props) {
+      const { theme, size, level } = props;
+      const classes = computed(() => {
         return {
           [`gulu-theme-${theme}`]: theme,
-          [`gulu-size-${size}`]: size
-        }
-      })
-      return {classes}
-      // 注意:这个地方的要加return 不能忘记
-    }
+          [`gulu-size-${size}`]: size,
+          [`gulu-level-${level}`]: level,
+        };
+      });
+      return { classes };
+    },
   };
 </script>
 <style lang="scss">
@@ -42,6 +39,7 @@
   $color: #333;
   $blue: #40a9ff;
   $radius: 4px;
+  $red: red;
   .gulu-button {
     box-sizing: border-box;
     height: $h;
@@ -56,9 +54,8 @@
     border: 1px solid $border-color;
     border-radius: $radius;
     box-shadow: 0 1px 0 fade-out(black, 0.95);
-    /*如果不知道fade-out可以搜一下scss的fade out函数*/
+    transition: background 250ms;
     & + & {
-      /*这句话的意思是gulu-button+gulu-button,也就是除了第一个gulu-button的剩下所有gulu-button*/
       margin-left: 8px;
     }
     &:hover,
@@ -68,39 +65,83 @@
     }
     &:focus {
       outline: none;
-      // 这句话在fireFox中兼容不是很好，所以写了下面一段
     }
     &::-moz-focus-inner {
-      /*上面这句话是为了兼容FireFox*/
       border: 0;
     }
-
-    &.gulu-theme-link{
+    &.gulu-theme-link {
       border-color: transparent;
       box-shadow: none;
       color: $blue;
-      &:hover,&:focus{
+      &:hover,
+      &:focus {
         color: lighten($blue, 10%);
       }
     }
-    &.gulu-theme-text{
+    &.gulu-theme-text {
       border-color: transparent;
       box-shadow: none;
       color: inherit;
-      &:hover,&:focus{
-        background: darken(white, 5%);;
+      &:hover,
+      &:focus {
+        background: darken(white, 5%);
       }
     }
-    &.gulu-theme-button{
-      &.gulu-size-big{
-        font-size: 24px;
-        height: 48px;
-        padding: 0 16px
+    &.gulu-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px;
+    }
+    &.gulu-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+    &.gulu-theme-button {
+      &.gulu-level-main {
+        background: $blue;
+        color: white;
+        border-color: $blue;
+        &:hover,
+        &:focus {
+          background: darken($blue, 10%);
+          border-color: darken($blue, 10%);
+        }
       }
-      &.gulu-size-small{
-        font-size: 12px;
-        height: 20px;
-        padding: 0 4px;
+      &.gulu-level-danger {
+        background: $red;
+        border-color: $red;
+        color: white;
+        &:hover,
+        &:focus {
+          background: darken($red, 10%);
+          border-color: darken($red, 10%);
+        }
+      }
+    }
+    &.gulu-theme-link {
+      &.gulu-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
+      }
+    }
+    &.gulu-theme-text {
+      &.gulu-level-main {
+        color: $blue;
+        &:hover,
+        &:focus {
+          color: darken($blue, 10%);
+        }
+      }
+      &.gulu-level-danger {
+        color: $red;
+        &:hover,
+        &:focus {
+          color: darken($red, 10%);
+        }
       }
     }
   }
