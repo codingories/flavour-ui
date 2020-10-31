03b42377c16bd1a610e9a63f60be4cfc56cb829d
朱骏杰 <ories.zhu@mochain.vip>
2020-10-31 09:44:47 +0800

    features样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,71 +6,81 @@
       </svg>
     </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
     <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 
 <script lang="ts">
-  import { inject, Ref } from 'vue'
+  import {
+    inject,
+    Ref
+  } from "vue";
   export default {
-    setup(){
-      const menuVisible =  inject<Ref<boolean>>('menuVisible'); // get, 获取xxx
-      const toggleMenu = ()=>{
-        menuVisible.value = !menuVisible.value
-        console.log('fuck',menuVisible.value)
+    setup() {
+      const menuVisible = inject < Ref < boolean >> ("menuVisible"); // get
+      const toggleMenu = () => {
+        menuVisible.value = !menuVisible.value;
       };
-      return {toggleMenu}
-    }
-  }
+      return {
+        toggleMenu
+      };
+    },
+  };
 </script>
+
 <style lang="scss" scoped>
-$color: #007974;
-.topnav {
-  color: $color;
-  display: flex;
-  padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 10;
-  justify-content: center;
-  align-items: center;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-    > svg {
-      width: 32px;
-      height: 32px;
-    }
-  }
-  > .menu {
+  $color: #007974;
+  .topnav {
+    color: $color;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
+    padding: 16px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
+    >.logo {
+      max-width: 6em;
+      margin-right: auto;
+      >svg {
+        width: 32px;
+        height: 32px;
+      }
+    }
+    >.menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      >li {
+        margin: 0 1em;
+      }
+    }
+    >.toggleAside {
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+      display: none;
+    }
+    @media (max-width: 500px) {
+      >.menu {
+        display: none;
+      }
+      >.logo {
+        margin: 0 auto;
+      }
+      >.toggleAside {
+        display: inline-block;
+      }
     }
   }
-  > .toggleAside {
-    width: 24px;
-    height: 24px;
-    background: red;
-    position: absolute;
-    left: 16px;
-    top: 50%;
-    transform: translateY(-50%);
-    display: none;
-  }
-  @media (max-width:800px) {
-    > .menu{display: none;}
-    > .logo{margin: 0 auto;}
-    > .toggleAside {
-      display: inline-block;
-    }
-  }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="topnavAndBanner">
-      <Topnav></Topnav>
+      <Topnav />
       <div class="banner">
-        <h1>flavour UI</h1>
-        <h2>一个厉害的UI框架</h2>
+        <h1>轱辘UI</h1>
+        <h2>一个厉害的 UI 框架</h2>
         <p class="actions">
           <a href="https://github.com">GitHub</a>
           <router-link to="/doc">开始</router-link>
@@ -12,33 +12,87 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
-
   </div>
 </template>
+
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
+  import Topnav from "../components/Topnav.vue";
   export default {
-    components: {Topnav}
-  }
+    components: {
+      Topnav
+    },
+  };
 </script>
+
 <style lang="scss" scoped>
   $green: #02bcb0;
   $border-radius: 4px;
   $color: #007974;
   .topnavAndBanner {
-    background: rgb(227,255,253);
-    background: linear-gradient(145deg, rgba(227,255,253,1) 0%, rgba(183,233,230,1) 100%);
-    clip-path: ellipse(80% 50% at 50% 40%);
+    background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+    clip-path: ellipse(80% 60% at 50% 40%);
+  }
+  .features {
+    margin: 64px auto;
+    width: 400px;
+    @media (min-width: 800px) {
+      width: 800px;
+    }
+    @media (min-width: 1200px) {
+      width: 1200px;
+    }
+    >ul {
+      display: flex;
+      flex-wrap: wrap;
+      >li {
+        width: 400px;
+        margin: 16px 0;
+        display: grid;
+        justify-content: start;
+        align-content: space-between;
+        grid-template-areas:
+            "icon title"
+            "icon text";
+        grid-template-columns: 80px auto;
+        grid-template-rows: 1fr auto;
+        >svg {
+          grid-area: icon;
+          width: 64px;
+          height: 64px;
+        }
+        >h3 {
+          grid-area: title;
+          font-size: 28px;
+        }
+        >p {
+          grid-area: text
+        }
+      }
+    }
   }
   .banner {
     color: $color;
@@ -47,25 +101,19 @@
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    > .actions{
+    >.actions {
       padding: 8px 0;
       a {
-        margin:0 8px;
+        margin: 0 8px;
         background: $green;
         color: white;
         display: inline-block;
         padding: 8px 24px;
         border-radius: $border-radius;
-        &:hover{
+        &:hover {
           text-decoration: none;
         }
       }
-    }
-  }
-  .features {
-    > svg {
-      width: 64px;
-      height: 64px;
     }
   }
 </style>
