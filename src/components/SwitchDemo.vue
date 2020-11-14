a875d24ae1782f48bcccbbb2cdfd91a6a8d4e94b
朱骏杰 <ories.zhu@mochain.vip>
2020-11-14 15:27:19 +0800

    小优化，直接用component渲染switchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo"></component>
         <div class="demo-actions">
           <Button>查看代码</Button>
         </div>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo />
+        <component :is="Switch2Demo"></component>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -39,8 +39,6 @@
   } from 'vue'
   export default {
     components: {
-      Switch1Demo,
-      Switch2Demo,
       Switch,
       Button
     },
