6141b8c3cab030814db5d5336331ab95310a694f
朱骏杰 <ories.zhu@mochain.vip>
2020-11-14 13:27:01 +0800

    使用markdown

--- plugins/md.ts
+++ plugins/md.ts
@@ -3,19 +3,13 @@
 import path from 'path'
 // @ts-ignore
 import fs from 'fs'
-// @ts-ignore
 import marked from 'marked'
 
 
-
-
-
-
-
 const mdToJs = str => {
-  const content = JSON.stringify(marked(str))
+  const content = JSON.stringify(marked(str));
   return `export default ${content}`
-}
+};
 
 export function md() {
   return {
@@ -23,8 +17,8 @@ export function md() {
       async ({ app }) => {
         app.use(async (ctx, next) => { // koa
           if (ctx.path.endsWith('.md')) {
-            ctx.type = 'js'
-            const filePath = path.join(process.cwd(), ctx.path)
+            ctx.type = 'js';
+            const filePath = path.join(process.cwd(), ctx.path);
             ctx.body = mdToJs(fs.readFileSync(filePath).toString())
           } else {
             await next()
